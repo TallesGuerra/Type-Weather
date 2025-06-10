a88524f8720eab6691454b3778c9b34aa6c5459d
talles <talles-guerra@hotmail.com>
2025-06-10 16:37:28 +0100

    vite bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Altere esta linha
+  base: '/Type-Weather/', // 👈 importante
 })
