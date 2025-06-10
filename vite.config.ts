f8d8de908da09c68e7978a6ca7fdd790947a0fa7
talles <talles-guerra@hotmail.com>
2025-06-10 16:44:31 +0100

    bugs catch

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Type-Weather/',
   plugins: [react()],
-  base: '/Type-Weather/', // 👈 importante
 })
