1ac59ba7481a29022c6cae5d92c45eaac6839b63
talles <talles-guerra@hotmail.com>
2025-06-11 15:02:57 +0100

    package.json and vite.config updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Type-Weather/',
   plugins: [react()],
-  build: {
-    cssCodeSplit: false
+  base: '/',
+  preview: {
+    host: '0.0.0.0',
+    port: 4173
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173
   }
 })
